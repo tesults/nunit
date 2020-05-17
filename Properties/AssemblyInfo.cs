5c0119bfb52a65bb5c208092609c9572846c63ef
Ajeet Dhaliwal <ajeet@tesults.com>
2020-05-17 19:58:59 +0100

    Assembly description update

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -6,12 +6,12 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Tesults.NUnit")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tesults NUnit Listener")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Tesults")]
 [assembly: AssemblyProduct("Tesults.NUnit")]
 [assembly: AssemblyCopyright("Copyright ©  2019")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("Tesults")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
